533ae8ca904236494cf49151c6d91585972d4cbb
Grain6888 <146320184+Grain6888@users.noreply.github.com>
2024-09-19 13:15:15 +0900

    事前準備課題が完了

--- WinOGL/WinOGL/WinOGLView.h
+++ WinOGL/WinOGL/WinOGLView.h
@@ -3,44 +3,54 @@
 //
 
 #pragma once
+#include <gl/GL.h>
 
 
-class CWinOGLView : public CView
-{
+class CWinOGLView : public CView {
 protected: // シリアル化からのみ作成します。
-	CWinOGLView() noexcept;
-	DECLARE_DYNCREATE(CWinOGLView)
+    CWinOGLView () noexcept;
+    DECLARE_DYNCREATE (CWinOGLView)
 
 // 属性
 public:
-	CWinOGLDoc* GetDocument() const;
+    CWinOGLDoc* GetDocument () const;
 
 // 操作
 public:
 
 // オーバーライド
 public:
-	virtual void OnDraw(CDC* pDC);  // このビューを描画するためにオーバーライドされます。
-	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+    virtual void OnDraw (CDC* pDC);  // このビューを描画するためにオーバーライドされます。
+    virtual BOOL PreCreateWindow (CREATESTRUCT& cs);
 protected:
 
 // 実装
 public:
-	virtual ~CWinOGLView();
+    virtual ~CWinOGLView ();
 #ifdef _DEBUG
-	virtual void AssertValid() const;
-	virtual void Dump(CDumpContext& dc) const;
+    virtual void AssertValid () const;
+    virtual void Dump (CDumpContext& dc) const;
 #endif
 
 protected:
 
 // 生成された、メッセージ割り当て関数
 protected:
-	DECLARE_MESSAGE_MAP()
+    DECLARE_MESSAGE_MAP ()
+public:
+    afx_msg void OnLButtonDown (UINT nFlags, CPoint point);
+private:
+    HGLRC m_hRC;
+public:
+    afx_msg int OnCreate (LPCREATESTRUCT lpCreateStruct);
+    afx_msg void OnDestroy ();
+    afx_msg BOOL OnEraseBkgnd (CDC* pDC);
 };
 
 #ifndef _DEBUG  // WinOGLView.cpp のデバッグ バージョン
-inline CWinOGLDoc* CWinOGLView::GetDocument() const
-   { return reinterpret_cast<CWinOGLDoc*>(m_pDocument); }
+inline CWinOGLDoc* CWinOGLView::GetDocument () const
+{
+    return reinterpret_cast<CWinOGLDoc*>(m_pDocument);
+}
 #endif
 
