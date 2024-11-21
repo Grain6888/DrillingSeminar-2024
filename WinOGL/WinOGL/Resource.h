d782db4ce1972d8930630322a0eada13819b3241
Grain6888 <146320184+Grain6888@users.noreply.github.com>
2024-11-22 00:11:05 +0900

    アプリケーションの終了ボタンを追加

--- WinOGL/WinOGL/Resource.h
+++ WinOGL/WinOGL/Resource.h
@@ -21,13 +21,14 @@
 #define ID_DeleteAll                    32796
 #define ID_DELETE_ALL                   32797
 #define ID_BUTTON32798                  32798
+#define ID_BUTTON32799                  32799
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        310
-#define _APS_NEXT_COMMAND_VALUE         32799
+#define _APS_NEXT_COMMAND_VALUE         32800
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
